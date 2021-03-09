bd1747783abb3e604fc9c336682576f9a8bd0ed0
an-le <ale@rsglab.com>
2021-03-09 10:41:44 -0500

    [EXT-XX]: sample extension

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const axios = require('axios')
+const axios = require('axios');
+const {getRenderResp} = require('./yelpResponse')
 
 app.use(express.json());
 
@@ -10,72 +11,20 @@ app.post('/', (req, res) => {
     res.json({
       "controls": [
         {
-          "number": {
-            "name": "rating",
-            "label": "Rating",
-            "description": "Choose rating number to show",
-            "min": 1,
-            "max": 5,
-            "default": 4
+          "boolean": {
+            "name": "show_rating",
+            "label": "Show Rating",
+            "description": "Showing business rating",
+            "default": true
           }
         },
-        {
-          "enum": {
-            "name": "favorite_color",
-            "label": "Favorite Color",
-            "description": "What is your favorite color?",
-            "options": [
-              {
-                "label": "Blue",
-                "value": "blue"
-              }
-            ],
-            "default": "blue"
-          }
-        }
       ]
     })
   }
   else {
-    // let num_biz = req.body.controlValues.num_biz
-
-    res.send({
-        type: 'TEXT',
-        properties: {
-          text: {
-            type: 'doc',
-            content: [
-              {
-                type: 'text',
-                text: 'Hello world!',
-              },
-            ],
-          },
-        },
-    })
+   getRenderResp().then(data => {
+      res.send(data)})
   }
-})
-
-let imgSrc = '';
-let API_KEY = "-QPl11oKmKSmT7oKLPv_LfmA4-BCletMeVwD9f61KU_HMHwdJXc7Ga30WVC7DekqMtCC0REsmrv75hMStbCGj1NiWiYTGFFx1IDbcGbQcL1Kk4YL4i9wvCB580U9YHYx";
-
-let yelpREST = axios.create({
-  baseURL: "https://api.yelp.com/v3/",
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-    "Content-type": "application/json",
-  }
-})
-
-yelpREST("/businesses/search", {
-  params: {
-    location: "brooklyn, NY",
-    term: "landscaping",
-    limit: 5
-  }
-}).then(({data})=> {
-  let {businesses} = data
-  businesses.forEach(b=>imgSrc=b.image_url)
 })
 
 app.listen(port, () => {
---
+++ yelpResponse.js
@@ -0,0 +1,152 @@
+const axios = require('axios');
+const API_KEY = "-QPl11oKmKSmT7oKLPv_LfmA4-BCletMeVwD9f61KU_HMHwdJXc7Ga30WVC7DekqMtCC0REsmrv75hMStbCGj1NiWiYTGFFx1IDbcGbQcL1Kk4YL4i9wvCB580U9YHYx";
+
+let yelpREST = axios.create({
+  baseURL: "https://api.yelp.com/v3/",
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-type": "application/json",
+  }
+})
+
+async function getBizData() {
+  try {
+    const response = await yelpREST(`https://api.yelp.com/v3/businesses/search?term=eleven madison&location=new york`)
+    let id_res = await response
+    const data = await yelpREST(`https://api.yelp.com/v3/businesses/${id_res.data.businesses[0].id}`)
+    let biz_data = await data.data
+    return biz_data
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+async function getRenderResp() {
+  const biz_data = await getBizData()
+  const {image_url, url, rating, review_count, price, location} = biz_data
+  const star_icon_url = "https://img.icons8.com/fluent/48/000000/star.png"
+
+  return ({
+    "type":"STACK_LAYOUT",
+    "children":[
+      {
+        "type":"IMAGE",
+        "properties":{
+          "alt":"business image",
+          "src":`${image_url}`,
+          "href":`${url}`
+        }
+      },
+      {
+        "type": "STACK_LAYOUT",
+        "children": [
+          {
+            "type": "TEXT",
+            "properties": {
+              "text": 
+                {
+                  "type": 'doc',
+                  "content": [{
+                    "type": 'heading',
+                    "content": [
+                      {
+                        "type": 'text',
+                        "text": `${biz_data.name}`
+                      },
+                    ]
+                  },
+                ],
+              },
+            },
+          },
+          {
+            "type": "TEXT",
+            "properties": {
+              "text": {
+                "type": 'doc',
+                "content": [
+                  {
+                    "type": 'paragraph',
+                    "content": [
+                      {
+                        "type": 'text',
+                        "text": `${location.address1}, ${location.city}, ${location.state}`
+                      },
+                    ],
+                  }
+                ]
+              }
+            }
+          },
+          {
+            "type": "CLUSTER_LAYOUT",
+            "children": [
+              {
+                "type": "CLUSTER_LAYOUT",
+                "properties": {
+                  "spacing": 0,
+                },
+                "children": [
+                {
+                  "type": "TEXT",
+                  "properties": {
+                    "text": {
+                      "type": 'doc',
+                      "content": [
+                        {
+                          "type": 'text',
+                          "text": `${rating}`
+                        }
+                      ]
+                    }
+                  }
+                },
+                {
+                  "type": "IMAGE",
+                  "properties": {
+                    "alt":"star icon",
+                    "src":`${star_icon_url}`,
+                    "width": 20,
+                    "maxHeight": 20
+                  }
+                },
+                ]
+              },
+              {
+                "type": "TEXT",
+                "properties": {
+                  "text": {
+                    "type": 'doc',
+                    "content": [
+                      {
+                        "type": 'text',
+                        "text": `${review_count} reviews`
+                      },
+                    ]
+                  }
+                }
+              },
+              {
+                "type": "TEXT",
+                "properties": {
+                  "text": {
+                    "type": 'doc',
+                    "content": [
+                      {
+                        "type": 'text',
+                        "text": `${price}`
+                      },
+                    ]
+                  }
+                }
+              },
+            ]
+          },// end of outer cluster layout
+        ]
+      },
+    ] // end of all outer children
+  })
+}
+exports.getRenderResp = getRenderResp;
+
+
